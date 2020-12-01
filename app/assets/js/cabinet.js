c960dea3e9413efc8c8b6b5227227a90444e11a6
reygasai <ReyCODE@yandex.ru>
2020-12-01 18:46:58 +0300

    fixes issue #2, #3, #4

--- app/assets/js/cabinet.js
+++ app/assets/js/cabinet.js
@@ -1,12 +1,20 @@
 export default class Cabinet {
     constructor() {
-        this.skinContainer = document.querySelector('#skin-container');
+        this.skinContainer = document.getElementById('skin-container');
 
-        this.skinPath = this.skinContainer.getAttribute('skin-path');
-        this.cloakPath = this.skinContainer.getAttribute('cloak-path');
+        //this.skinPath = this.skinContainer.getAttribute('skin-path');
+        //this.cloakPath = this.skinContainer.getAttribute('cloak-path');
     }
     
     init() {
-      
+        let skinViewer = new viewer({
+            canvas: this.skinContainer,
+            width: 300,
+            height: 400,
+            skin: "../../media/images/skin.png"
+        });
+        
+
+        console.log(skinViewer);
     }
 }
